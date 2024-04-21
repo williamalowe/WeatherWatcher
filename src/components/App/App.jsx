397c620263a39d1101393b7b67cef951505921dc
WilliamALowe <WilliamLoweDev@Gmail.com>
2024-04-21 13:53:20 +1000

    func: add useWeatherData hook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,16 @@
 import Cloudbg from '../../../../WeatherWise/v2/src/components/Cloudbg/Cloudbg';
 import styles from './App.module.css';
+import useWeatherData from '../../hooks/useWeatherData/useWeatherData';
 
 const App = () => {
+  const { weatherData, error, loading } = useWeatherData();
+
   return (
     <main className={styles.app}>
       <div className={styles.background}>
         <Cloudbg />
       </div>
+      
     </main>
   )
 }
